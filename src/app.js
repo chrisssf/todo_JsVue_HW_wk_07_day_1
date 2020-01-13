f9677e45fbb7546e74f1642506910ddb86fab169
Chris Fraser <chrisssfraser@gmail.com>
2020-01-13 17:13:18 +0000

    need to give user feedback and do CSS

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   // console.log("loaded JS file");
-  
+
   new Vue({
     el: '#app',
-    data: {},
-    methods: {}
+    data: {
+      toDo: [
+        {name: "buy toilet paper", priority: "low"},
+        {name: "eat caaaaake", priority: "low"},
+        {name: "reflect on life choices", priority: "low"}
+      ],
+      newTask: {name: "", priority: ""}
+    },
+    methods: {
+      addTask: function() {
+        this.toDo.push(this.newTask);
+        this.newTask = {name: "", priority: ""};
+      },
+      givePriority: function(task) {
+        if (task.priority === "low"){
+          return "low"
+        } else {
+          return "high"
+        }
+      }
+    }
   });
 });
