278bf7df921d73131928817450092e04a8357ca4
Chris Fraser <chrisssfraser@gmail.com>
2020-01-13 16:01:01 +0000

    created files

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // console.log("loaded JS file");
+  
+  new Vue({
+    el: '#app',
+    data: {},
+    methods: {}
+  });
+});
